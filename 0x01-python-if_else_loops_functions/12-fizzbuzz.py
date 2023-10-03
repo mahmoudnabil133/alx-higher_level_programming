626cf319370f306d0c3e2a8e83d961b1bdf5fe32
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-04 02:45:16 +0300

    if_elif_loops_functions

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+            continue
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+            if i == 100:
+                break
+            elif i % 15 == 0:
+                print ("FizzBuzz", end=" ")
+                continue
+            else:
+                print(f"{i} ", end="")
+    
