f9d495c68870c1d47b22455d04f430db7d0658ea
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-08 01:30:55 +0300

    lists_toubles

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
-        return None
+        return new_list
     new_list[idx] = element
     return new_list
