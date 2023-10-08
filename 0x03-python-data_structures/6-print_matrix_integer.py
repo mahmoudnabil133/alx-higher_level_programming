00d3b3e1ba1d8765e0e07dd676f323b4ff6e42b2
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-08 03:15:50 +0300

    lists_toubles

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        y = 0
+        for j in i:
+            y += 1
+            if y != len(i):
+                print("{:d}".format(j), end=" ")
+            else:
+                print("{:d}".format(j), end="")
+        print("$")
