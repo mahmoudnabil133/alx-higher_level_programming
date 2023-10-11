ec118954c4ec5cdc6bb3dd28fbdc20b9b8d7db27
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-12 02:30:30 +0300

    set dic & lamda_map()_filter()_reduce()

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -11,3 +11,7 @@ print_sorted_dictionary(new_dict)
 
 print("--")
 print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
