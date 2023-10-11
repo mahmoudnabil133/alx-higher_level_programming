1e19367a03255b0221e6696af3917f7d0789a0c3
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-12 01:11:44 +0300

    set dic & lamda_map()_filter()_reduce()

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     mat2 = matrix.copy()
     for i in range(len(matrix)):
-        mat2[i] = list(map(lambda x : x ** 2, matrix[i]))
+        mat2[i] = list(map(lambda x: x ** 2, matrix[i]))
     return (mat2)
