c18c988887bd36f032a9e83204b548cf88efc1c6
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-12 02:56:56 +0300

    set dic & lamda_map()_filter()_reduce()

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return (None)
-    max = a_dictionary[list(a_dictionary)[0]]
-    for i in a_dictionary:
-        if a_dictionary[i] > max:
-            max = a_dictionary[i]
-            ret = i
-    return (ret)
+    if a_dictionary and len(a_dictionary):
+        max = a_dictionary[list(a_dictionary)[0]]
+        for i in a_dictionary:
+            if a_dictionary[i] > max:
+                max = a_dictionary[i]
+                ret = i
+        return (ret)
+    return (None)
