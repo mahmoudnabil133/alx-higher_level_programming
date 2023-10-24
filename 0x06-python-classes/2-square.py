13b3169804b09a9f7ea26bd133cc2cb859536019
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-24 23:44:00 +0300

    Classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" define a class"""
+
+
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an intiger")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
