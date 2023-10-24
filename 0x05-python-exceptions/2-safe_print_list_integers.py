e84770758dfa6cca508e8487b4fb747d664f0e6c
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-24 08:26:45 +0300

    Exceptions try & except

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    a, b = 0, 0
+    while a < x:
+        try:
+            print("{:d}".format(my_list[a]), end="")
+            b += 1
+        except (IndexError, ValueError):
+            pass
+        a += 1
+    print()
+    return b
