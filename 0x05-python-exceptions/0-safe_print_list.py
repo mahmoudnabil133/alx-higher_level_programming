f777d23f1ac9a6ac3cf3d9fe8e9bc529b11937bf
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-24 08:30:26 +0300

    Exceptions try & except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print("{:d}".format(my_list[i]), end="")
-        print("")
-    except IndexError:
-        i -= 1
-        print("")
-    return (i + 1)
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
+    print()
+    return printed
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+
+
 def safe_print_list_integers(my_list=[], x=0):
-    a, b = 0, 0
-    while a < x:
+    """
+    prints a list of anything, but only prints the integers
+    Returns the amount of integers printed
+    """
+    i = 0
+    printed = 0
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[a]), end="")
-            b += 1
-        except (IndexError, ValueError):
-            pass
-        a += 1
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+        except (ValueError, TypeError):
+            continue
     print()
-    return b
+    return printed
