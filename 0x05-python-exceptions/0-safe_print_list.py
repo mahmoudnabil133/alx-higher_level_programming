21c77fc345fd8cfc9c9b64e44e72f5dc585c1a26
mahmoudnabil133 <hodanabil144@gmail.com>
2023-10-24 07:47:24 +0300

    Exceptions try & except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,4 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
         print("")
     except IndexError:
-        pass
+        i -=1
+        print("")
+    return (i + 1)
