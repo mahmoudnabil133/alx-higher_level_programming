ddb8903d92795f345ef516f97b80dcb6d8a495e4
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-06 15:39:37 +0200

    inheritancd

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""my module"""
+
+
+def is_same_class(obj, a_class):
+    """ my function"""
+
+    if type(obj) != a_class:
+        return False
+    else:
+        return True
