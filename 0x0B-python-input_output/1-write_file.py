70303389b02e158c6f0cf1a6bfa613874f2a7d9f
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-07 15:42:38 +0200

    I/O-FILE

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """define a function"""
-    with open(filename, "w", encodein="utf-8") as file:
+    with open(filename, "w") as file:
         return file.write(text)
