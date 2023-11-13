479d63ac597ef42d31bb7df8941b353a4b333f58
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-13 19:04:20 +0200

    every thing almost acircle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """define Rectangle class"""
 
+
     def __init__(self, width, height, x=0, y=0, id=None):
         '''define init'''
         super().__init__(id)
