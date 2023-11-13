0b20e4c1d8e31a6a2e28e59117b9a889e47ca098
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-13 19:12:08 +0200

    every thing almost acircle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     '''define a class'''
+
     def __init__(self, size, x=0, y=0, id=None):
         '''class constructor'''
         super().__init__(size, size, x, y, id)
