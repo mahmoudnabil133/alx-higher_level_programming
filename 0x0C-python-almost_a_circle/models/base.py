ceab95c5b2c6387baab1bb45fab16780566b8efb
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-13 17:56:38 +0200

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,17 +2,19 @@
 """define module"""
 from json import dumps, loads
 
+
 class Base:
     """define a class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """ our init function"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id =  Base.__nb_objects
-    
+            self.id = Base.__nb_objects
+
     @staticmethod
     def to_json_string(list_dictionaries):
         '''Jsonifies a dictionary so it's quite rightly and longer.'''
