2d2457379504e9bb1befdb02bb163c8eb7a6283a
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-13 19:09:59 +0200

    every thing almost acircle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,16 +2,17 @@
 """define a module"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     '''define a class'''
     def __init__(self, size, x=0, y=0, id=None):
         '''class constructor'''
         super().__init__(size, size, x, y, id)
-    
+
     @property
     def size(self):
         return self.width
-    
+
     @size.setter
     def size(self, value):
         self.validate_int('width', value, False)
@@ -21,8 +22,9 @@ class Square(Rectangle):
 
     def __str__(self):
         '''majic str function'''
-        return '[Square] ({}) {}/{} - {}' .format(self.id, self.x, self.y, self.size)
-    
+        return '[Square] ({}) {}/{} - {}' .\
+            format(self.id, self.x, self.y, self.size)
+
     def update(self, *args, **kwargs):
         j = 1
         for i in args:
@@ -36,7 +38,7 @@ class Square(Rectangle):
             elif j == 4:
                 self.y = i
             j += 1
-        
+
         if args:
             pass
         else:
@@ -50,7 +52,7 @@ class Square(Rectangle):
                     self.y = value
                 elif key == 'id':
                     self.id = value
-    
+
     def to_dictionary(self):
         dic = {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
         return dic
