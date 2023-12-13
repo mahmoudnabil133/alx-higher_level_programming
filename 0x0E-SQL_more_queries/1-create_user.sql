96146abb157bf914475519feee3591eefc745774
mahmoudnabil133 <hodanabil144@gmail.com>
2023-12-13 21:17:51 +0200

    more in sql joins and constrains

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- create user
 CREATE USER IF NOT EXISTS 'user_od_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL
+GRANT ALL PRIVILEGE PRIVILEGES
 ON *.*
 TO 'user_od_1'@'localhost'
 FLUSH PRIVILEGES;
