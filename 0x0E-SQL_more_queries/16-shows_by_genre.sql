2098fd2f163dffdadccc863b1f7ad439397f8de1
mahmoudnabil133 <hodanabil144@gmail.com>
2023-12-14 19:27:35 +0200

    more in sql joins and constrains

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,4 @@
+-- 3 tables join t1 LEFT_JOIN t2 LEFT_JOIN t3
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
