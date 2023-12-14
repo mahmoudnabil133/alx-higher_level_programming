832073381b2be8835fdf28f1632c2413e47c9c48
mahmoudnabil133 <hodanabil144@gmail.com>
2023-12-14 16:25:00 +0200

    more in sql joins and constrains

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
 -- left join without intersection
 SELECT SH.title, SHG.genre_id
 FROM tv_shows AS SH LEFT JOIN tv_show_genres AS SHG ON SH.id = SHG.show_id
-WHERE SHG.genre_id is NULL;
+WHERE SHG.genre_id IS NULL;
