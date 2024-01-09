bb40078c19985aeb386cc0176dfb27b71beb4b2a
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-09 20:18:51 +0200

    JavaScript

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+function getSecMax (list) {
+  let max = parseInt(list[0]);
+  let SecMax = parseInt(list[0]);
+  for (let num of list) {
+    num = parseInt(num);
+    if (num > max) {
+      SecMax = max;
+      max = num;
+    }
+  }
+  return SecMax;
+}
+if (isNaN(process.argv[3])) {
+  console.log(0);
+} else {
+  const numbers = process.argv.slice(2, process.argv.length);
+  const result = getSecMax(numbers);
+  console.log(result);
+}
