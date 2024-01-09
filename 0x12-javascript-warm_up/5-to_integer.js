82fd01b856958d6957719c1a35ba75cfbd0ddd39
mahmoudnabil133 <hodanabil144@gmail.com>
2024-01-09 18:14:01 +0200

    JavaScript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number:', parseInt(process.argv[2]));
+  console.log('My number: ', parseInt(process.argv[2]));
 }
