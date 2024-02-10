75a06ce4a0e40ec2420f2e0543b643e8e4669765
mahmoudnabil133 <hodanabil144@gmail.com>
2024-02-10 22:58:49 +0200

    removed for c18

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const word of list) {
-  console.log(word);
-}
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const loop = process.argv[2];
-for (let i = 0; i < loop; i++) {
-  console.log('C is fun');
-}
