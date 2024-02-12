96677701118dc3cce785583a2ade4571df5ff966
mahmoudnabil133 <hodanabil144@gmail.com>
2024-02-12 16:58:52 +0200

    done js task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   let max = parseInt(process.argv[2]);
-  let secMax = Number.NEGATIVE_INFINITY; 
+  let secMax = Number.NEGATIVE_INFINITY;
   for (let i = 3; i < process.argv.length; i++) {
     const num = parseInt(process.argv[i]);
     if (num > max) {
