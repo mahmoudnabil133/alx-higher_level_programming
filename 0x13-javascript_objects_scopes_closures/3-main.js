660efcd89e6fbf3728dbbbcde771fdff6fa7385a
mahmoudnabil133 <hodanabil144@gmail.com>
2024-02-13 17:18:24 +0200

    done 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-main.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const Rectangle = require('./3-rectangle');
+r1 = new Rectangle(10, 5);
+r1.print()
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(h) || isNaN(w)) {
+      // do nothing
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+};
+  
