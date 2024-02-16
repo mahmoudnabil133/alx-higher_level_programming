dc3132080d5966fd6088c56f7cba79c11a2106aa
mahmoudnabil133 <hodanabil144@gmail.com>
2024-02-16 11:46:04 +0200

    done changes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,10 @@
 "docs of the file"
 import sys
 import MySQLdb
-db = MySQLdb.connect(host = 'localhost', port = '3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM `states`")
-rows = cur.fetchall()
-for r in rows:
-    print(r)
+if __name__ == '__main__':
+    db = MySQLdb.connect(host = 'localhost', port = '3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states`")
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
--- 0x0F-python-object_relational_mapping/git.sh
+++ 0x0F-python-object_relational_mapping/git.sh
@@ -1,3 +1,3 @@
-git add $1
-git commit -m "$1"
+git add .
+git commit -m "done changes"
 git push
