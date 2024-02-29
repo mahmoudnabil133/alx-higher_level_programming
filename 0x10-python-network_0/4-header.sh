318d2f80f27917b347c0c5cd4194070733bbe9b0
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-01 00:50:02 +0200

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # add header variable
-curl  -H "X-School-User-Id: 98" $1
+curl  -sH "X-School-User-Id: 98" $1
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # add boddy variable
-curl  -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
+curl  -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
