e7131c79ba72f87b237f98f022fb88559b417a09
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-01 00:16:03 +0200

    2-

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# size of response
-x=$(curl -Is $1| grep -i HTTP | cut -f2 -d ' ')
-if [ "$x" == "200" ]; then 
-    curl -sI $1
+# ret
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then 
+    curl -sL $1
 fi
