159ed0c609f88f6fae8a924b6604165321be6e2e
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-02 22:20:03 +0200

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
         if len(sys.argv) <= 1:
             val = ""
         else:
-            val = sys.argv[2]
+            val = sys.argv[1]
         req = requests.post("http://0.0.0.0:5000/search_user", {"q": val})
         res = req.json()
         if not res:
             print("No result")
         else:
-            print("[{}] {}".format(res["id"], res["name"]))
-    except ValueError as e:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except Exception as e:
         print("Not a valid JSON")
