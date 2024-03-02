92763e629071851a889e278ddd8917f023883c3a
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-02 18:53:42 +0200

    3-error_code.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,6 +9,7 @@ import sys
 if __name__ == "__main__":
     dec = {}
     dec['email'] = sys.argv[2]
-    encoded_data = parse.urlencode(dec).encode("ascii")
+    encoded_data = parse.urlencode(dec).encode("utf-8")
     with request.urlopen(sys.argv[1], data=encoded_data) as res:
         body = res.read().decode("utf-8")
+        print(body)
