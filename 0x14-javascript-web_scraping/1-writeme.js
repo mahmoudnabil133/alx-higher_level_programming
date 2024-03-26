05b9958f1aafc39f9be9c21df288aa5be123bc1a
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-26 23:11:43 +0200

    write

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that read content of file
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filePath, data, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
