6592666bd7307f228304cacb1da89df8a89e08e5
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-27 01:00:40 +0200

    tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// get response status code
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const dec = {};
+    for (const todo of data) {
+      const id = todo.userId;
+      if (dec[id] === undefined) {
+        dec[id] = 0;
+      }
+      if (todo.completed) {
+        dec[id] += 1;
+      }
+    }
+    console.log(dec);
+  }
+});
